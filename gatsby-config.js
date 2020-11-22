81bc67444c7d1e707528d4c01d7bd0c6c28edf04
Mohammed Asker <zetoanime@gmail.com>
2020-11-22 11:44:17 +0300

    add gatsby feed plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,6 +57,59 @@ module.exports = {
 				trackingId: `UA-174924847-1`,
 			},
 		},
+		{
+			resolve: `gatsby-plugin-feed`,
+			options: {
+				query: `
+				{
+				  site {
+					siteMetadata {
+					  title
+					  description
+					  siteUrl
+					  site_url: https://www.mohammedasker.com/
+					}
+				  }
+				}
+			  `,
+				feeds: [
+					{
+						serialize: ({ query: { site, allMarkdownRemark } }) => {
+							return allMarkdownRemark.edges.map((edge) => {
+								return Object.assign({}, edge.node.frontmatter, {
+									description: edge.node.excerpt,
+									date: edge.node.frontmatter.date,
+									url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+									guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+									custom_elements: [{ "content:encoded": edge.node.html }],
+								});
+							});
+						},
+						query: `
+					{
+					  allMarkdownRemark(
+						sort: { order: DESC, fields: [frontmatter___date] },
+					  ) {
+						edges {
+						  node {
+							excerpt
+							html
+							fields { slug }
+							frontmatter {
+							  title
+							  date
+							}
+						  }
+						}
+					  }
+					}
+				  `,
+						output: "/rss.xml",
+						title: "Your Site's RSS Feed",
+					},
+				],
+			},
+		},
 		`gatsby-plugin-sitemap`,
 		{
 			resolve: `gatsby-plugin-manifest`,
