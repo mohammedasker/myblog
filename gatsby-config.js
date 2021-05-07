afde7ec156a1e3f485182ce240ad564458f28ce6
mohammedasker <zetoanime@gmail.com>
2021-05-07 08:43:39 +0300

    removed social media links from header

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,97 +1,89 @@
 require(`dotenv`).config({
-	path: `.env`,
+  path: `.env`
 });
 
 const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
 
 module.exports = {
-	siteMetadata: {
-		siteTitle: `Mohammed Asker`,
-		siteTitleAlt: `Mohammed Asker - Front-end Developer, Blogger, and Freelancer`,
-		siteHeadline: `Mohammed Asker - Front-end Developer, Blogger, and Freelancer`,
-		siteUrl: `https://mohammedasker.com`,
-		siteDescription: `I'm a Front-End Developer building a beautiful, mobile-responsive,
+  siteMetadata: {
+    siteTitle: `Mohammed Asker`,
+    siteTitleAlt: `Mohammed Asker - Front-end Developer, Blogger, and Freelancer`,
+    siteHeadline: `Mohammed Asker - Front-end Developer, Blogger, and Freelancer`,
+    siteUrl: `https://mohammedasker.com`,
+    siteDescription: `I'm a Front-End Developer building a beautiful, mobile-responsive,
 		and accessible websites and web applications.`,
-		siteLanguage: `en`,
-		author: `@MooAsker`,
-		siteImage: `/banner.jpg`,
-	},
-	plugins: [
-		{
-			resolve: `@lekoarts/gatsby-theme-minimal-blog`,
-			// See the theme's README for all available options
-			options: {
-				navigation: [
-					{
-						title: `Blog`,
-						slug: `/blog`,
-					},
-					{
-						title: `About`,
-						slug: `/about`,
-					},
-					{
-						title: `Contact`,
-						slug: `/contact`,
-					},
-				],
-				externalLinks: [
-					{
-						name: `Twitter`,
-						url: `https://twitter.com/mooasker`,
-					},
-					{
-						name: `GitHub`,
-						url: `https://www.github.com/mohammedasker/`,
-					},
-					{
-						name: `LinkedIn`,
-						url: `https://linkedin.com/in/mohdasker`,
-					},
-				],
-			},
-		},
-		{
-			resolve: `gatsby-plugin-google-analytics`,
-			options: {
-				trackingId: `UA-174924847-1`,
-			},
-		},
-		`gatsby-plugin-sitemap`,
-		{
-			resolve: `gatsby-plugin-manifest`,
-			options: {
-				name: `Mohammed Asker - Front-end Developer, Blogger, and Freelancer`,
-				short_name: `Mohammed Asker`,
-				description: `I'm a Front-End Developer building a beautiful, mobile-responsive,
+    siteLanguage: `en`,
+    author: `@MooAsker`,
+    siteImage: `/banner.jpg`
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      // See the theme's README for all available options
+      options: {
+        navigation: [
+          {
+            title: `Blog`,
+            slug: `/blog`
+          },
+          {
+            title: `About`,
+            slug: `/about`
+          },
+          {
+            title: `Contact`,
+            slug: `/contact`
+          }
+        ],
+        externalLinks: [
+          {
+            name: ``,
+            url: ``
+          }
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-174924847-1`
+      }
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Mohammed Asker - Front-end Developer, Blogger, and Freelancer`,
+        short_name: `Mohammed Asker`,
+        description: `I'm a Front-End Developer building a beautiful, mobile-responsive,
 				and accessible websites and web applications.`,
-				start_url: `/`,
-				background_color: `#fff`,
-				theme_color: `#6B46C1`,
-				display: `standalone`,
-				icons: [
-					{
-						src: `/android-chrome-192x192.png`,
-						sizes: `192x192`,
-						type: `image/png`,
-					},
-					{
-						src: `/android-chrome-512x512.png`,
-						sizes: `512x512`,
-						type: `image/png`,
-					},
-				],
-			},
-		},
-		`gatsby-plugin-offline`,
-		`gatsby-plugin-netlify`,
-		shouldAnalyseBundle && {
-			resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
-			options: {
-				analyzerMode: `static`,
-				reportFilename: `_bundle.html`,
-				openAnalyzer: false,
-			},
-		},
-	].filter(Boolean),
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#6B46C1`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`
+          }
+        ]
+      }
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false
+      }
+    }
+  ].filter(Boolean)
 };
